9399dd4888e1ea13f836995f2881a38342660372
89trillion-alimasi <alimasi@89trillion.com>
2021-07-06 16:14:59 +0800

    增加了统一管理的状态码

---
+++ controller/StatusErr.go
@@ -0,0 +1,45 @@
+package controller
+
+const (
+	ParameterBindingIsUnsuccessful = 400
+	ReceivingUserIsEmpty           = 401
+	SpecifyTheNumberOfRedemptions  = 402
+	Invalidgiftcodetype            = 404
+	GiftCodeDescription            = 405
+	PleaseEnterAValidTime          = 406
+	IncorrectTimeFormat            = 407
+	PackageContent                 = 408
+	CreateGiftCodeFaied            = 409
+	CreatedSuccessfully            = 201
+	GiftCodeHasExpired             = 410
+	GiftCodeErr                    = 411
+	FailedToClaim                  = 412
+	Successful                     = 200
+	InvalidTime                    = 403
+	InsertionFailed                = 413
+)
+
+var statusText = map[int]string{
+	ParameterBindingIsUnsuccessful: "绑定参数未成功",
+	ReceivingUserIsEmpty:           "请指定领取用户",
+	SpecifyTheNumberOfRedemptions:  "指定可兑换次数",
+	Invalidgiftcodetype:            "礼品码类型不合法",
+	GiftCodeDescription:            "请输入礼品码描述信息",
+	PleaseEnterAValidTime:          "请输入有效期",
+	IncorrectTimeFormat:            "输入的礼品码有效期格式不正确",
+	PackageContent:                 "请输入礼包内容",
+	CreateGiftCodeFaied:            "创建礼品码失败",
+	CreatedSuccessfully:            "成功",
+	GiftCodeHasExpired:             "礼品码输入错误或礼品码已过期",
+	GiftCodeErr:                    "请输入礼品码",
+	FailedToClaim:                  "领取失败",
+	Successful:                     "成功",
+	InvalidTime:                    "有效时间小于当前本地的时间",
+	InsertionFailed:                "插入礼品码失败",
+}
+
+// StatusText returns a text for the HTTP status code. It returns the empty
+// string if the code is unknown.
+func StatusText(code int) string {
+	return statusText[code]
+}
